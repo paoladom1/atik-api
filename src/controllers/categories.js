0b838575fd16303fd5d3c68288e102050cdb95da
Paola Dominguez <paoladomdiaz@gmail.com>
2020-07-12 20:22:54 -0600

    adds products of category route

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,73 +1,94 @@
-import db from "../models";
-import category from "../models/category";
+import db from '../models';
+import category from '../models/category';
 const Product = db.producto;
 const Category = db.categoria;
 
 const findAllCategories = (req, res) => {
-    Category.findAll({
-        include: [{ model: Product, as: "products", nested: false }],
+  Category.findAll({
+    include: [{model: Product, as: 'products', nested: false}],
+  })
+    .then(categories => {
+      res.json(categories);
     })
-        .then((categories) => {
-            res.json(categories);
-        })
-        .catch((err) => res.status(500).json({ err }));
+    .catch(err => res.status(500).json({err}));
 };
 
 const findById = (req, res) => {
-    const id = req.params.categoryId;
+  const id = req.params.categoryId;
 
-    Category.findOne({
-        where: { id },
-        include: [{ model: Product, as: "products", nested: false }],
+  Category.findOne({
+    where: {id},
+    include: [{model: Product, as: 'products', nested: false}],
+  })
+    .then(category => {
+      res.json({category});
     })
-        .then((category) => {
-            res.json({ category });
-        })
-        .catch((err) => res.status(500).json({ err }));
+    .catch(err => res.status(500).json({err}));
+};
+
+const findCategoryProductsById = (req, res) => {
+  const id = req.params.categoryId;
+
+  Category.findOne({
+    where: {id},
+    include: [{model: Product, as: 'products', nested: false}],
+  })
+    .then(category => {
+      const {products} = category;
+      res.json({products});
+    })
+    .catch(err => res.status(500).json({err}));
 };
 
 const create = (req, res) => {
-    let { name, imageUrl } = req.body;
+  let {name, imageUrl} = req.body;
 
-    const newCategory = {
-        name,
-        imageUrl,
-    };
+  const newCategory = {
+    name,
+    imageUrl,
+  };
 
-    Category.create(newCategory)
-        .then((category) => res.json(category))
-        .catch((err) => res.status(500).json({ err }));
+  Category.create(newCategory)
+    .then(category => res.json(category))
+    .catch(err => res.status(500).json({err}));
 };
 
 const update = (req, res) => {
-    const { name, imageUrl } = req.body;
+  const {name, imageUrl} = req.body;
 
-    const id = req.params.categoryId;
+  const id = req.params.categoryId;
 
-    Category.update(
-        {
-            name,
-            imageUrl,
-        },
-        { where: { id } }
-    )
-        .then((category) => res.json(category))
-        .catch((err) => res.status(500).json({ err }));
+  Category.update(
+    {
+      name,
+      imageUrl,
+    },
+    {where: {id}},
+  )
+    .then(category => res.json(category))
+    .catch(err => res.status(500).json({err}));
 };
 
 const updateAll = (req, res) => {
-    const { imageUrl } = req.body;
+  const {imageUrl} = req.body;
 
-    Category.update(
-        {
-            imageUrl,
-        },
-        {
-            where: { imageUrl: null },
-        }
-    )
-        .then((categories) => res.json(categories))
-        .catch((err) => res.status(500).json({ err }));
+  Category.update(
+    {
+      imageUrl,
+    },
+    {
+      where: {imageUrl: null},
+    },
+  )
+    .then(categories => res.json(categories))
+    .catch(err => res.status(500).json({err}));
 };
 
-export { updateAll, update, create, findAllCategories, findById };
+export {
+  updateAll,
+  update,
+  create,
+  findAllCategories,
+  findById,
+  findCategoryProductsById,
+};
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,34 +1,37 @@
-import passport from "passport";
-import config from "../config/config";
-import { allowOnly } from "../services/routesHelper";
+import passport from 'passport';
+import config from '../config/config';
+import {allowOnly} from '../services/routesHelper';
 import {
-    update,
-    updateAll,
-    create,
-    findById,
-    findAllCategories,
-} from "../controllers/categories";
+  update,
+  updateAll,
+  create,
+  findById,
+  findAllCategories,
+  findCategoryProductsById,
+} from '../controllers/categories';
 
-module.exports = (app) => {
-    app.post(
-        "/api/category/create",
-        passport.authenticate("jwt", { session: false }),
-        allowOnly(config.accessLevels.admin, create)
-    );
+module.exports = app => {
+  app.post(
+    '/api/category/create',
+    passport.authenticate('jwt', {session: false}),
+    allowOnly(config.accessLevels.admin, create),
+  );
 
-    app.put(
-        "/api/categories/:categoryId/",
-        passport.authenticate("jwt", { session: false }),
-        allowOnly(config.accessLevels.admin, update)
-    );
+  app.put(
+    '/api/categories/:categoryId/',
+    passport.authenticate('jwt', {session: false}),
+    allowOnly(config.accessLevels.admin, update),
+  );
 
-    app.put(
-        "/api/categories/",
-        passport.authenticate("jwt", { session: false }),
-        allowOnly(config.accessLevels.admin, updateAll)
-    );
+  app.put(
+    '/api/categories/',
+    passport.authenticate('jwt', {session: false}),
+    allowOnly(config.accessLevels.admin, updateAll),
+  );
 
-    app.get("/api/categories", findAllCategories);
+  app.get('/api/categories', findAllCategories);
 
-    app.get("/api/categories/:categoryId", findById);
+  app.get('/api/categories/:categoryId', findById);
+
+  app.get('/api/categories/:categoryId/products', findCategoryProductsById);
 };
