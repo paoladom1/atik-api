b0d0e0d520dc72cdcb7ae3018ab1c64a90e900cd
Paola Dominguez <paoladomdiaz@gmail.com>
2020-07-14 01:43:06 -0600

    update: create user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,63 +1,59 @@
-import passport from 'passport';
-import config from '../config/config';
-import {allowOnly} from '../services/routesHelper';
+import passport from "passport";
+import config from "../config/config";
+import { allowOnly } from "../services/routesHelper";
 import {
-  create,
-  login,
-  me,
-  findAllUsers,
-  findById,
-  update,
-  deleteUser,
-} from '../controllers/user';
+    create,
+    login,
+    me,
+    findAllUsers,
+    findById,
+    update,
+    deleteUser,
+} from "../controllers/user";
 
-module.exports = app => {
-  // create a new user
-  app.post(
-    '/api/users/create',
-    passport.authenticate('jwt', {session: false}),
-    allowOnly(config.accessLevels.admin, create),
-  );
+module.exports = (app) => {
+    // create a new user
+    app.post("/api/users/create", create);
 
-  // user login
-  app.post('/api/users/login', login);
+    // user login
+    app.post("/api/users/login", login);
 
-  // get current user
-  app.get('/api/me', passport.authenticate('jwt', {session: false}), me);
+    // get current user
+    app.get("/api/me", passport.authenticate("jwt", { session: false }), me);
 
-  //retrieve all users
-  app.get(
-    '/api/users',
-    passport.authenticate('jwt', {
-      session: false,
-    }),
-    allowOnly(config.accessLevels.admin, findAllUsers),
-  );
+    //retrieve all users
+    app.get(
+        "/api/users",
+        passport.authenticate("jwt", {
+            session: false,
+        }),
+        allowOnly(config.accessLevels.admin, findAllUsers)
+    );
 
-  // retrieve user by id
-  app.get(
-    '/api/users/:userId',
-    passport.authenticate('jwt', {
-      session: false,
-    }),
-    allowOnly(config.accessLevels.client, findById),
-  );
+    // retrieve user by id
+    app.get(
+        "/api/users/:userId",
+        passport.authenticate("jwt", {
+            session: false,
+        }),
+        allowOnly(config.accessLevels.client, findById)
+    );
 
-  // update a user with id
-  app.put(
-    '/api/users/:userId',
-    passport.authenticate('jwt', {
-      session: false,
-    }),
-    allowOnly(config.accessLevels.client, update),
-  );
+    // update a user with id
+    app.put(
+        "/api/users/:userId",
+        passport.authenticate("jwt", {
+            session: false,
+        }),
+        allowOnly(config.accessLevels.client, update)
+    );
 
-  // delete a user
-  app.delete(
-    '/api/users/:userId',
-    passport.authenticate('jwt', {
-      session: false,
-    }),
-    allowOnly(config.accessLevels.admin, deleteUser),
-  );
+    // delete a user
+    app.delete(
+        "/api/users/:userId",
+        passport.authenticate("jwt", {
+            session: false,
+        }),
+        allowOnly(config.accessLevels.admin, deleteUser)
+    );
 };
